7b4c7dea925843f58ca58d6455c059e9f60f126e
patrick odey <patrickjoseph2121@gmail.com>
2022-02-13 13:33:32 +0100

    Initial Commit

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const path = require('path')
 
-router.get('/', (req, res) => {
+router.get('/api', (req, res) => {
     res.sendFile(path.join(__dirname, '../../view', 'index.html'))
 })
 
